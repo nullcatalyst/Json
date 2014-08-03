75eefba1f5af24b49a11d988e85ad395fb189ef6
Scott Bennett <scottbennett912@gmail.com>
2014-08-03 13:11:41 -0500

    Created some basic test cases

--- JsonTests/JsonTests.swift
+++ JsonTests/JsonTests.swift
@@ -8,6 +8,7 @@
 
 import Cocoa
 import XCTest
+import Json
 
 class JsonTests: XCTestCase {
     
@@ -21,16 +22,46 @@ class JsonTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
+    func testString() {
+        var string: Json = "test\nString"
+        
+        // Verify that the parser is escaping characters correctly
+        if let parse = Json.parse("\"test\\nString\"") {
+            XCTAssertEqual(parse, string, "The entry with key \"boolean\" should have been removed")
+        } else {
+            XCTFail("Should be able to parse the json string type successfully")
         }
     }
     
+    func testArrayAccess() {
+        var array: Json = [0, 1, 2]
+        
+        // Check array elements
+        XCTAssert(array[0]? == 0, "Element 0 should equal 0")
+        XCTAssert(array[1]? == 1, "Element 1 should equal 1")
+        XCTAssert(array[2]? == 2, "Element 2 should equal 2")
+        
+        // Remove from the array
+        array[1] = nil
+        XCTAssert(array[1]? == 2, "Element 1 should equal 2")
+        
+        // Key access to an array
+        XCTAssert(array["test"]? == nil, "Should not be able to use a key to access an array")
+    }
+    
+    func testObjectAccess() {
+        var object: Json = ["boolean": true, "number": 4]
+        
+        XCTAssertEqual(object, ["boolean": true, "number": 4], "Assert that the object literal conversion worked properly")
+        object["boolean"] = nil
+        XCTAssertEqual(object, ["number": 4], "The entry with key \"boolean\" should have been removed")
+    }
+    
+    func testParser() {
+        if let object = Json.parse("{\"answer\": 42, \"question\": \"6 * 9\"}") {
+            XCTAssertEqual(object, ["answer": 42, "question": "6 * 9"], "Assert that the object was parsed correctly")
+        } else {
+            XCTFail("{\"answer\": 42} should have been parsed as a valid object")
+        }
+    }
 }
